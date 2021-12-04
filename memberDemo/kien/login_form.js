1015af011883f1267820f5dc78309caf08a6b048
Kien <nguyenkien18102002@gmail.com>
2021-12-04 23:45:40 +0700

    .

--- memberDemo/kien/login_form.js
+++ memberDemo/kien/login_form.js
@@ -1,41 +1,5 @@
+window.addEventListener('load',function() {
+  const subVariantContainer = document.querySelector('')
 
-function Validator(options) {
 
-  var formElement = document.querySelector(options.form);
-
- 
-
-  if (formElement) {
-
-    options.rules.forEach(function (rule) {
-      var inputElement = formElement.querySelector(rule.selector);
-
-      if (inputElement) {
-        inputElement.onblur = function () {
-          console.log('blur ' + rule.selector);
-        }
-      }
-
-    });
-  }
-}
-
-// // định nghĩa có rules
-Validator.isRequired = function(selector) {
-  return {
-    selector: selector,
-    text: function () {
-
-    }
-  };
-}
-
-Validator.isPassword =  function(selector){
-  return {
-    selector: selector,
-    text: function () {
-      
-    }
-  };
-
-}
+});
